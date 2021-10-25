e003af8834af003288085dfceeb819d27d1af6e4
Josephinejegan <josephinejegan5@gmail.com>
2021-10-25 00:25:28 -0700

    project1

---
+++ src/org/com/test/GitEx.java
@@ -0,0 +1,12 @@
+package org.com.test;
+
+public class GitEx {
+public void empid() {
+	System.out.println("Jegan");
+
+}
+
+public static void main(String[] args) {
+	System.out.println("welcome");
+}
+}
