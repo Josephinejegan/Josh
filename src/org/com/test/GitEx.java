efed34fc4d2a61ac62fbd27b5691c0e620b4ac2b
Josephinejegan <josephinejegan5@gmail.com>
2021-10-25 00:54:49 -0700

    pencil

--- src/org/com/test/GitEx.java
+++ src/org/com/test/GitEx.java
@@ -2,9 +2,11 @@ package org.com.test;
 
 public class GitEx {
 public void empid() {
-	System.out.println("Jegan");
+	System.out.println("Jegan");} private void empName() {
+		System.out.println("mani");
 
-}
+	}
+
 
 public static void main(String[] args) {
 	System.out.println("welcome");
